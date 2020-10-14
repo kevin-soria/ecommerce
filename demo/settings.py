09ddecd688f1fa3910a6c722021b67cd3508e3bd
Kevin Soria <Kevin.Soria@outlook.com>
2020-10-13 23:58:10 -0400

    added to backens

--- demo/settings.py
+++ demo/settings.py
@@ -81,3 +81,10 @@ if ENVIRONMENT == 'production':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # auth
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
