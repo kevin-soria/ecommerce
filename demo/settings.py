14f1c4aaba2583de5f5c373c294a04b75abe9e62
Kevin Soria <Kevin.Soria@outlook.com>
2020-10-13 23:50:34 -0400

    projec tread and debug set to true

--- demo/settings.py
+++ demo/settings.py
@@ -2,10 +2,10 @@ import os
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = True
+DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = 'False'                   
 
 INSTALLED_APPS = [
     'django.contrib.admin',
