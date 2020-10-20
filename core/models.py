42212a57d49cd7be0087b899d97df8fd2725e8b9
Kevin Soria <Kevin.Soria@outlook.com>
2020-10-19 23:52:33 -0400

    class ini3 and and firs methods to class

--- core/models.py
+++ core/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max(max_length=100))
+    
+
 class OrderItem(models.Model):
     pass
+
 
 class Order(model.Model):
     pass
