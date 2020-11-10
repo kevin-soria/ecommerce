e9986a2bbc4e1576081c05ba6fbf54ac370a935d
Kevin Soria <Kevin.Soria@outlook.com>
2020-11-09 23:58:14 -0500

    adding item list to view with request as param

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render  j 
+from .models import Item
 
 # Create your views here.
+def item_list(request):
+    return render(request, "item_list.html", context=)
